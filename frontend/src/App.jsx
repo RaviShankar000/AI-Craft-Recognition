83de14d7b7102f77c90e2a2c0411cb21fa3387cf
RaviShankar000 <ravishankar82923@gmail.com>
2026-01-31 10:47:21 +0530

    chore: initialize frontend using Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to React + Vite</h1>
+      <p>Start building your amazing app!</p>
+    </div>
+  )
+}
+
+export default App
